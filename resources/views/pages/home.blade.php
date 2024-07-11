8e17ed44a227fead97063f8c4193105c074a5ab2
engelbertushendro10 <engelbertushendro@gmail.com>
2024-07-11 14:28:56 +0700

    layout

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,22 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <title>Document</title>
-</head>
-<body>
+@extends('layout.main')
 
-@include('layout.navbar')
 
-<div class="container mt-4">
-    <h1>WEB framework BOE</h1>
-</div>
+@section('content')
 
-      {{-- scripts --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+
+    <h1>Home WEB framework BOE</h1>
+
+
+@endsection
--- resources/views/pages/projects/index.blade.php
+++ resources/views/pages/projects/index.blade.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-<link rel="stylesheet" href="{{asset('style.css')}}">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+@extends('layout.main')
 
-</head>
-<body>
-    @include('layout.navbar')
-    <h1>Selamat Datang Di halaman Project Saya</h1>
+@section('content')
 
-    {{-- script --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+
+<h1>Halaman index project</h1>
+
+@endsection
